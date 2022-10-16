c5b4889bf3986a1aff698dc1d44cdef8f0cc6a82
favour01216 <favouradesiyan2@gmail.com>
2022-10-16 17:20:04 -0230

    initial commit including organized files and some Readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# Team-Profile-Generator
+
+## Licensing:
+
+[![license](https://img.shields.io/badge/license-MIT-blue)](https://shields.io)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Additional Info](#additional-info)
+
+## Description:
+
+The Inquirer module from Node.js was used to develop this application, which generates a team profile based on user input and displays the data on a freshly created html page with a style sheet.
+
+## Installation:
+
+use inquirer from your command line to answer questions abour the project you are workking on.
+
+## Usage:
+
+use inquirer from your command line to answer questions abour the project you are workking on.
+
+## License:
+
+MIT
+
+## Contribution:
+
+undefined
+
+## Testing:
+
+undefined
+
+## Additional Info:
+
+- Github: [Favour01216](https://github.com/Favour01216)
+- Email: Favouradesiyan2@gmail.com
---
+++ __tests__/Employee.test.js
@@ -0,0 +1,38 @@
+// Employee constructor
+const Employee = require("../lib/Employee");
+// creates employee object
+test("Can instantiate Employee instance", () => {
+  const e = new Employee();
+  expect(typeof e).toBe("object");
+});
+
+//get id from getId()
+//for name
+test("Testing name.", () => {
+  const name = "James";
+  const employeeInstance = new Employee(name);
+  expect(employeeInstance.name).toBe(name);
+});
+//for ID
+test("gets employee ID", () => {
+  const employee = new Employee("Favour", 60, "favouradesiyan2@gmail.com");
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+test("gets employee email", () => {
+  const employee = new Employee("Favour", 60, "Favouradesiyan2@gmail.com");
+
+  expect(employee.getemail()).toEqual(
+    expect.stringContaining(employee.email.toString())
+  );
+});
+//gets role of employee
+test("Gets role.", () => {
+  const returnValue = "Employee";
+  const employeeInstance = new Employee(
+    "Favour",
+    65,
+    "Favouradesiyan2@gmail.com"
+  );
+  expect(employeeInstance.getRole()).toBe(returnValue);
+});
